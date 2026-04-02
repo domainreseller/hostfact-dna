5823d6ca19d479f3d19192eee8f7daa479a20fc5
bakcay <bakcay@live.com>
2026-04-02 16:28:39 +0300

    Bump version to 2.0.1

--- domainnameapi/version.php
+++ domainnameapi/version.php
@@ -1,8 +1,8 @@
 <?php
 $version['name']            = "DomainNameApi";
-$version['api_version']     = "3.0.0"; // API version of the registrar
+$version['api_version']     = "3.0.1"; // API version of the registrar
 $version['date']            = "2026-04-01"; // Last modification date
-$version['version']         = "2.0.0"; // Version released for HostFact
+$version['version']         = "2.0.1"; // Version released for HostFact
 $version['autorenew']       = true; // AutoRenew is default?  true | false
 $version['handle_support']  = true; // Handles are supported? true | false
 $version['cancel_direct']   = false; // Possible to terminate domains immediately?  true | false
